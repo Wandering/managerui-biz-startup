710cdead975542513c38f2b656a56c885ccdc008
王永海 <yhwang@thinkjoy.cn>
2015-05-13 11:07:31 +0800

    新增格式化js与标签处理js

--- mubs-admin-war/src/main/webapp/assets/js/biz/biz-common.js
+++ mubs-admin-war/src/main/webapp/assets/js/biz/biz-common.js
@@ -26,40 +26,37 @@
         $('#edit_modal').modal('show');
     }
 
-    //日期 long 显示为 yyyy-MM-dd hh:mm:ss
-    function formatdate(cellvalue){
-        if(cellvalue == null || cellvalue=='' ||cellvalue ==0){
-            return "";
-        }
-        var date= new Date(cellvalue);
-        var month = (date.getMonth()+1);
-        var dates = date.getDate();
-        var hour = date.getHours();
-        var minutes = date.getMinutes();
-        var seconds = date.getSeconds();
-        if(month < 10){
-            month = "0"+month;
-        }
-        if(dates < 10){
-            dates = "0"+dates;
-        }
-        if(hour < 10){
-            hour = "0"+hour;
-        }
-        if(minutes < 10){
-            minutes = "0"+minutes;
-        }
-        if(seconds < 10){
-            seconds = "0"+seconds;
-        }
-
-
-        return date.getFullYear()+"-"+month+"-"+dates+" "+hour+":"+minutes+":"+seconds;
-    }
-
     //同步获取 jqgrid编辑页面 select 组件值
     function dynGetData(ajaxUrl){
         var returnStr = "";
+        $.ajaxSettings.async = false;
+        $.getJSON(ajaxUrl, function(result){
+            //debugger;
+            if("0000000" == result.rtnCode){
+                for(i = 0; i < result.bizData.rows.length; i++){
+                    if(i < result.bizData.rows.length - 1) {
+                        returnStr += result.bizData.rows[i].id + ":" + result.bizData.rows[i].name + ";";
+                    } else {
+                        returnStr += result.bizData.rows[i].id + ":" + result.bizData.rows[i].name;
+                    }
+                }
+
+            } else {
+                alert("request error");
+            }
+
+        });
+        $.ajaxSettings.async = true;
+        return returnStr;
+    }
+    /**
+     * 同步获取 jqgrid编辑页面 select 组件值
+     * @param ajaxUrl
+     * @returns {string}
+     * @author yhwang
+     */
+    function dynGetDataIncludeBlank(ajaxUrl){
+        var returnStr = "0"+":"+"请选择"+";";
         $.ajaxSettings.async = false;
         $.getJSON(ajaxUrl, function(result){
             //debugger;
@@ -151,3 +148,4 @@
         //ctl = ctl.replace( /<br />&nbsp;$/, '' );
         return ctl;
     }
+
